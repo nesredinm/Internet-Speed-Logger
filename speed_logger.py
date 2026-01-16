1a13289f2d3b7334d6a001d988981ab1e98fbc50
Nesre <nesredin.m11@gmail.com>
2026-01-16 15:58:27 +0300

    set it to run incertain time interval

--- speed_logger.py
+++ speed_logger.py
@@ -1,22 +1,29 @@
 import speedtest
 from datetime import datetime
 import csv
+import time
 
-# Initialize speedtest
-st = speedtest.Speedtest(secure=True)
-st.get_best_server()
+def test_speed():
+    # Initialize speedtest
+    st = speedtest.Speedtest(secure=True)
+    st.get_best_server()
 
-# Run
-download_speed = st.download()/1_000_000
-upload_speed = st.upload()/1_000_000
-ping = st.results.ping
+    # Run
+    download_speed = st.download()/1_000_000
+    upload_speed = st.upload()/1_000_000
+    ping = st.results.ping
 
-# Timestamp
-timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    # Timestamp
+    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-print("\n", download_speed,"\n",  upload_speed,"\n", ping, "\n", timestamp)
+    print("\n", download_speed,"\n",  upload_speed,"\n", ping, "\n", timestamp)
 
-# Save ot csv
-with open("internet_speed_log.csv", "a", newline="") as file:
-    writer = csv.writer(file)
-    writer.writerow([timestamp, round(download_speed, 2), round(upload_speed, 2), ping])
+    # Save ot csv
+    with open("internet_speed_log.csv", "a", newline="") as file:
+        writer = csv.writer(file)
+        writer.writerow([timestamp, round(download_speed, 2), round(upload_speed, 2), ping])
+
+# Run every 10 min
+while True:
+    test_speed()
+    time.sleep(5 * 60)
